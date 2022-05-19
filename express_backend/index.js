f18c7a429d27912659bde0c3689091c3934ede5d
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-05-19 20:47:18 +0200

    added message log-message in backend and experimental code to establish a db connection

---
+++ express_backend/index.js
@@ -0,0 +1,27 @@
+/*import express from 'express';
+import { MongoClient } from 'mongodb';
+
+const app = express();
+
+async function connect() {
+
+  const client = new MongoClient('mongodb://localhost:10095');
+
+  await client.connect();
+
+  const db = client.db('melodice');
+  collection = db.collection('Song');
+
+  if (collection){
+      message = "Great Sucess";
+    }
+    else message = "Oh no";
+
+  return message;
+}
+
+console.log("jojo");
+console.log( connect());
+*/
+
+console.log("can y'all hear me?");
