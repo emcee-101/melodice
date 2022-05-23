e136e61a66e3669a5b28f551943a3e1b095a170a
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-05-23 22:19:18 +0200

    initial code for lyrics api

--- express_backend/api_lyrics/main.js
+++ express_backend/api_lyrics/main.js
@@ -1,2 +1,32 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
 
-function requestLyrics(id){}
+
+
+async function connect() {
+    if (collection) {
+      return collection;
+    }
+    const client = new MongoClient('mongodb://root:root@database:27017');
+  
+    await client.connect();
+  
+    const db = client.db('melodice');
+    collection = db.collection('Song');
+  
+    let message = null
+    if (collection){
+        message = "Great Sucess";
+      }
+      else message = "Oh no";
+  
+      console.log(message);
+      return collection;
+  }
+
+export async function requestLyrics(id){
+    const collection = await connect();
+    const lyricsObj = await collection.findOne({_id: id});
+
+
+}
