e3f3171b7496f0086c27e4457b903704e9ab33da
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-05-28 18:42:48 +0200

    new test case working

--- express_backend/api_lyrics/main.js
+++ express_backend/api_lyrics/main.js
@@ -4,68 +4,60 @@ import mongoose from 'mongoose'
 
 
 
+
 export async function connectMongoose() {
 
     var db = await mongoose.connect('mongodb://root:root@database:27017');
     await mongoose.connection.dropDatabase();
-      console.log("Connection Successful!");
+    console.log("Connection Successful!");
      
-      // define Schema
-      const songSchema = new mongoose.Schema({name: String, type: String, author: Number, lyrics: String, audiofile: String});
+  };
+
+export async function requestLyrics(id){
+  
+    await connectMongoose();
+  
+    // define Schema for a Song
+    var songSchema = new mongoose.Schema({name: String, type: String, author: Number, lyrics: String, audiofile: String});
    
-      // compile schema to model
-      const Song = mongoose.model('Song', songSchema); 
-   
-      // a document instance
-      let newSong = new Song({ name: 'I love Mongoose', type: 'l', author: 1, lyrics: 'Hili Hello wir sind alle froh', audiofile: 'amogus.mp3'});
-   
-      // save model to database
-      await newSong.save(function (err, track) {
+    // compile schema to model
+    var Song = mongoose.model('Song', songSchema); 
+
+
+    /*
+    // TESTCASE
+    // create a document instance
+    let newSong = new Song({name: 'Is love Mongoose', type: 'l', author: 1, lyrics: 'Hili Hello wir sind alle froh', audiofile: 'amogus.mp3'});
+
+    await newSong.save(function (err, track) {
+      if (err) return console.error(err);
+        console.log(track.name + " saved to colelction");
+        });
+
+
+    // TESTCASE PRINT ALL OBJECTS
+    // save model to database
+    await newSong.save(function (err, track) {
         if (err) return console.error(err);
         console.log(track.name + " saved to colelction");
         const docs = Song.find();
         console.log(docs);
+        //Song.find({}, function(err, docs){console.log(docs)});
+        
+        Song.find({}).lean().exec(function(err, docs) {
+          console.log(docs);
+        });
+
+        //console.log(all + "hello btw");
       });
 
-      let searchForID = '1';
-      
+*/ 
 
 
-      //const query = Song.where({_id: searchForID});
-    
-      /*await query.findOne(function (err, song) {
-        if(err){
-          console.log(err);
-          return handleError(err);
-        }
-        if(song){
-          console.Log(song.lyrics);
-        })*/
-      
-    
-  };
-
-async function requestLyrics(){
-    
-  // Database Table for a Song is being defined here
+  Song.findById({_id: id}).lean().exec(function(err, docs) {
+    if (err) console.log(err); return err;
+    if (docs) return docs;
+  });
 
 
-  
-  await connectMongoose();
-  
-
-  searchForID = 1;
-
-  const query = Song.where({_id: searchForID});
-
-  query.findOne(function (err, song) {
-    if(err){
-      console.log(err);
-      return handleError(err);
-    }
-    if(song){
-      console.Log(song.lyrics);
-    }
-  }
-  );
-}
+};
