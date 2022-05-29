ca7bea27e43123f6637c3469bb96f943c2e2b592
mo0329zi <mohammad.zidane@fh-erfurt.de>
2022-05-29 11:41:28 +0200

    Update express_backend/index.js

--- express_backend/index.js
+++ express_backend/index.js
@@ -1,5 +1,8 @@
 import {init as startupLyrics} from './api_lyrics/startup.js'
 import {init as startupMatcher} from './api_matcher/startup.js'
+import {init as startupAudio} from './api_audio/startup.js'
+
 
 startupLyrics()
 startupMatcher()
+startupAudio()
