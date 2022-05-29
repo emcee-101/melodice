0d37814ccdf4589e7df0dcd96ea477390932f57a
mo0329zi <mohammad.zidane@fh-erfurt.de>
2022-05-29 11:25:13 +0200

    Deleted express_backend/api_lyrics/data_model.js

--- express_backend/api_lyrics/data_model.js
+++
@@ -1,4 +0,0 @@
-import mongoose from 'mongoose'
-
-export const  songSchema = new mongoose.Schema({name: String, type: ['audio', 'lyrics', 'both'], author: Number, lyrics: String, audiofile: String});
-export const Song = mongoose.model('Song', songSchema); 
