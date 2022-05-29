19f37663b0bd17192983eaeff6642c0dfeaf2667
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-05-29 12:30:44 +0200

    further work on lyrics api

--- express_backend/api_lyrics/main.js
+++ express_backend/api_lyrics/main.js
@@ -1,47 +1,22 @@
-import express from 'express';
-import mongoose from 'mongoose'
 import {Song} from "../common_db_schema/data_model.js";
 
 
 
-export async function connectMongoose() {
+export async function handleLyricsRequest(req, res){
 
-  await mongoose.connect('mongodb://root:root@database:27017');
-  await mongoose.connection.dropDatabase();
-  console.log("Connection Successful!");
-   
-};
+  Song.findById({_id: req.params.parameter})
+      .lean()
+      .exec(function(err, docs) {
 
-function handleLyricsRequest(req, res){
+        //http answer is being sent
+          if (err){console.log(err); res.status(404); res.send({})}
 
-  //await connectMongoose();
-
-  Song.findById({_id: req.params.id}).lean().exec(function(err, docs) {
-
-    if (err){console.log(err); res.status(404); res.send({})}
-    else {res.status(200); res.send({lyrics: docs.lyrics})};
+          else {res.status(200); res.send({lyrics: docs.lyrics})};
     
-    }
-  );
+      }
+    );
 };
 
-
-export function expressShizzle(){
-
-  const app = express();
-
-  app.get('/lyrics/:id', function(req, res){handleLyricsRequest(req, res)});
-    // /:id means req.params.id is accessible on the other end
-
-
-  //app.use('/lyrics', LyricsRequestor());
-
-  app.listen(10093, () => {
-    console.log('Lyrics Service is listening at http://localhost:10093');
-  });
-
-
-};
 
 
 export async function testCase(){
@@ -49,51 +24,15 @@ export async function testCase(){
   let newSong = new Song({name: 'Is love Mongoose', type: 'l', author: 1, lyrics: 'Hili Hello wir sind alle froh', audiofile: 'amogus.mp3'});
 
   await newSong.save(function (err, track) {
+
     if (err) return console.error(err);
+
         Song.find({}).lean().exec(function(err, docs) {
+
           console.log(docs);
+
         });
       });
 
   
 };
-
-
-
-/*
-
- 
-    // DEPRECATED
-
-export async function requestLyrics(id){
- 
-    // TESTCASE
-    // create a document instance
-    let newSong = new Song({name: 'Is love Mongoose', type: 'l', author: 1, lyrics: 'Hili Hello wir sind alle froh', audiofile: 'amogus.mp3'});
-
-    await newSong.save(function (err, track) {
-      if (err) return console.error(err);
-        console.log(track.name + " saved to colelction");
-        });
-
-
-    // TESTCASE PRINT ALL OBJECTS
-    // save model to database
-    await newSong.save(function (err, track) {
-        if (err) return console.error(err);
-        console.log(track.name + " saved to colelction");
-        const docs = Song.find();
-        console.log(docs);
-        //Song.find({}, function(err, docs){console.log(docs)});
-        
-        Song.find({}).lean().exec(function(err, docs) {
-          console.log(docs);
-        });
-
-        //console.log(all + "hello btw");
-      });
-
-  };
-}
-
-*/
--- express_backend/api_lyrics/startup.js
+++ express_backend/api_lyrics/startup.js
@@ -1,3 +1,18 @@
+
+import {initExpressServer,connectMongoose,clearDatabase} from '../common_base/common.js'
+import {handleLyricsrequest,testCase} from './main.js'
+
+// Some Method Signatures for Methods called here:
+//          initExpressServer(name, url_suffix, port, callback_function)
+//          handleLyricsRequest(req, res)
+
+
+// start Lyrics API
 export function init(){
-    console.log('dummy startup')
+    
+    connectMongoose()
+        .then(clearDatabase())
+        .then(testCase())
+        .then(initExpressServer('Lyrics', 'lyrics', 10093, handleLyricsrequest));
+
     }
