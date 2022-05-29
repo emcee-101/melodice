1b395b32bb4c84f2ec276ae6e13f0b1c5bf28968
mo0329zi <mohammad.zidane@fh-erfurt.de>
2022-05-29 11:24:33 +0200

    Update express_backend/index.js

--- express_backend/index.js
+++ express_backend/index.js
@@ -1,41 +1,5 @@
-import express from 'express';
-import { MongoClient } from 'mongodb';
+import {init as startupLyrics} from './api_lyrics/startup.js'
+import {init as startupMatcher} from './api_matcher/startup.js'
 
-let collection = null;
-
-const app = express();
-
-async function connect() {
-  if (collection) {
-    return collection;
-  }
-  const client = new MongoClient('mongodb://root:root@database:27017');
-
-  await client.connect();
-
-  const db = client.db('melodice');
-  collection = db.collection('Song');
-
-  let message = null
-  if (collection){
-      message = "Great Sucess";
-    }
-    else message = "Oh no";
-
-    console.log(message);
-}
-
-console.log("jojo");
-
-
-//console.log(connect());
-
-console.log("can y'all hear me?");
-
-
-
-console.log("TESTTEST");
-
-import {connectMongoose, expressShizzle, testCase} from './api_lyrics/main.js'
-
-connectMongoose().then(testCase()).then(expressShizzle());
+startupLyrics()
+startupMatcher()
