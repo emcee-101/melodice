0c89ef399ba880644fa686ca132fb0a7853edd1b
mo0329zi <mohammad.zidane@fh-erfurt.de>
2022-05-29 11:10:49 +0200

    Add common functions to reduce redunadcy

---
+++ express_backend/common_base/common.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose'
+import {DATABASE_URL, LISTEN_PORT} from "./config.js"
+
+
+
+
+export async function connectMongoose() {
+
+  await mongoose.connect(DATABASE_URL);
+  await mongoose.connection.dropDatabase();
+  const db = mongoose.connection;
+  db.on("error", (error) => console.log(error));
+  db.on("open", () => console.log("Connected to Database"));
+  console.log("LOG: Connection to DB was started");
+   
+};
+
+export async function clearDatabase() {
+
+  await mongoose.connection.dropDatabase();
+  console.log("Database dropped");
+   
+};
+
+export function expressShizzle(name, url_suffix, port, callback_function){
+
+  const app = express();
+
+  app.get(url_suffix, function(req, res){callback_function(req, res)});
+
+
+
+  app.listen(port, () => {
+    console.log(`${name} Service is listening at http://localhost:${LISTEN_PORT}/${url_suffix}`);
+  });
+
+
+};
