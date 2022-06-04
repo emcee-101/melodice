bc2ca499b765d065637cbf3279b12d6d95b6ee0d
mozi <mohammad.zidane@fh-erfurt.de>
2022-06-04 11:57:59 +0200

    adapted to the new common base

--- express_backend/api_matcher/main.js
+++ express_backend/api_matcher/main.js
@@ -1,34 +1,8 @@
-import express from "express";
-import mongoose from "mongoose";
 import { Song } from "../common_base/data_model.js";
 
-const DATABASE_URL = "mongodb://root:root@database:27017";
-const LISTEN_PORT = 10092;
 
-init_demon();
-connectMongoose();
-
-export async function connectMongoose() {
-  mongoose.connect(DATABASE_URL);
-  const db = mongoose.connection;
-  db.on("error", (error) => console.log(error));
-  db.on("open", () => console.log("Matcher connected to Database"));
-}
-
-export function init_demon() {
-  const app = express();
-  app.get("/matcher/:input", function (req, res) {
-    requestMatcher(req.params.input, res);
-  });
-  app.listen(LISTEN_PORT, () => {
-    console.log(
-      `Matcher Service is listening at http://localhost:${LISTEN_PORT}`
-    );
-  });
-}
-
-function requestMatcher(input, res) {
-  Song.findOne({ name: { $regex: input, $options: "i" } }, (err, docs) => {
+function requestMatcher(req, res) {
+  Song.findOne({ name: { $regex: req.params.id, $options: "i" } }, (err, docs) => {
     if (err) {
       console.log(err);
       res.status(404);
@@ -41,6 +15,6 @@ function requestMatcher(input, res) {
   });
 }
 
-export function matchSong(songName){
+export async function matchSong(songName){
   requestMatcher(songName)
 }
--- express_backend/api_matcher/startup.js
+++ express_backend/api_matcher/startup.js
@@ -1,3 +1,11 @@
+import {initExpressServer,connectMongoose,clearDatabase} from '../common_base/common.js'
+import {matchSong} from './main.js'
+const LISTEN_PORT = 10092;
+
 export function init(){
-    console.log('dummy startup')
+    connectMongoose()
+    .then(clearDatabase())
+    .then(testCase())
+    .then(initExpressServer('Matcher', '/matcher/:id', LISTEN_PORT, matchSong));
+
     }
