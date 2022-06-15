5b78c520c0421b442f47bb955fed505001a55f00
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-06-15 11:32:45 +0200

    add error handling at lyrics api

--- express_backend/api_lyrics/main.js
+++ express_backend/api_lyrics/main.js
@@ -5,16 +5,11 @@ import {Song} from "../common_base/data_model.js";
 export async function handleLyricsRequest(req, res){
 
   Song.findById({_id: req.params.parameter})
-      .lean()
-      .exec(function(err, docs) {
-
-        //http answer is being sent
-          if (err){console.log(err); res.status(404); res.send({})}
-
-          else {res.status(200); res.send({lyrics: docs.lyrics})};
-    
-      }
-    );
+      .exec((err, docs) => {
+          if(err){console.log(err); res.status(404); res.send({message: 'Track not found'})}
+          else{res.status(200); res.send({lyrics: docs.lean().lyrics})};
+       })
+       
 };
 
 
