7992feb8599141b57c1e135a8c870f90905150c7
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-06-16 18:43:57 +0200

    post api ready to be tested

--- express_backend/api_post_new/main.js
+++ express_backend/api_post_new/main.js
@@ -1,8 +1,10 @@
 import { connectMongoose } from "../common_base/common.js";
 import {Song} from "../common_base/data_model.js";
+import { fileURLToPath } from 'url'
+import path from 'path'
+import fs from 'fs'
 
-
-function testCase(){
+function testCases(){
 
     //export const  songSchema = new mongoose.Schema({name: String, type: ['audio', 'lyrics', 'both'], author: Number, lyrics: String, audiofile: String});
 
@@ -19,11 +21,41 @@ function testCase(){
 
 }
 
-async function saveAudioFile(){}
+function getAudioDir(){
+
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = dirname(__filename)
+
+    // its best if this solution gets replaced with a less hacky one
+    const __rootDir = path.basename(path.dirname(__dirname))
+    const __audioDir = __rootDir + '/api_audio/audio_files';
+
+    return __audioDir;
+}
+
+async function saveAudioFile(audioFileData, name){
+    
+    // buffer base64 received from request
+    const fileContents = Buffer.from(audioFileData, 'base64');
+    
+    // find out corresponding filename for data to be written
+    const audioFileName = `${getAudioDir()}+'/'+${name}.mp3`;
+    
+    writeFile(audioFileName, fileContents, function(err) {
+
+      if(err)console.log(err);
+      console.log(`file saved to ${audioFileName}`)
+
+      let fileNameToBeReferenced = name+'.mp3'
+      console.log(`the file will be noted in the db as the following name: ${fileNameToBeReferenced}`)
+      return fileNameToBeReferenced
+    
+    });
+}
 
 export async function requestPost(req, res){
 
-    let song, audiofilename;
+    let song, audiofilename, resMessage='';
 
     switch(req.body.typeOfPost){
         // add new Song in DB with only lyrics
@@ -33,13 +65,13 @@ export async function requestPost(req, res){
         
         // add new Song in DB with only audio
         case audio:
-            audiofilename = saveAudioFile(req.body.audiodata);
+            audiofilename = await saveAudioFile(req.body.audiodata, req.body.name);
             song = new Song({name: req.body.name, type: req.body.type, author: req.body.author, audiofile: audiofilename});
             break;
         
         // add new Song in DB with both
         case both:
-            audiofilename = saveAudioFile(req.body.audiodata);
+            audiofilename = await saveAudioFile(req.body.audiodata, req.body.name);
             song = new Song({name: req.body.name, type: req.body.type, author: req.body.author, audiofile: audiofilename, lyrics: req.body.lyrics});
             break;
 
@@ -47,33 +79,33 @@ export async function requestPost(req, res){
         case add:
             
             // find corresponding song
-            let doc = await Song.findOne({_id: req.body.id});
+            let song = await Song.findOne({_id: req.body.id}, (err)=>{console.log('document not found');resMessage='object to be updated not found,'})
 
             switch(req.body.addType){
 
                 case 'addLyrics':
-                    doc.lyrics = req.body.lyrics;
+                    song.lyrics = req.body.lyrics;
                     break;
+
                 case 'addAudio':
-                    audiofilename = saveAudioFile(req.body.audiodata);
-                    doc.audiofile = audiofilename;
+                    audiofilename = await saveAudioFile(req.body.audiodata, req.body.name);
+                    song.audiofile = audiofilename;
                     break;
                 
             }
-
-            doc.type = 'both';
-            song = doc;
-
+            
+            song.type = 'both';
+            
             break;
         }
         
         song.save((err, doc)=>{
             
             if(err){
-                res.send(400).send({message: 'saving song unsucessful'});
+                res.send(400).send({message: resMessage+'saving song unsucessful'});
             }
             else{
-                res.status(200).send({message: 'saving song sucessful'});
+                res.status(200).send({message: resMessage+'saving song sucessful'});
             }
         })
 
