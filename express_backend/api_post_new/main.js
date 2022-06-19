a9aea9a6a4a6607b40990606249da63f64f85f83
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-06-19 14:33:40 +0200

    remove unused test cases

--- express_backend/api_post_new/main.js
+++ express_backend/api_post_new/main.js
@@ -4,22 +4,6 @@ import { fileURLToPath } from 'url'
 import path from 'path'
 import fs from 'fs'
 
-function testCases(){
-
-    //export const  songSchema = new mongoose.Schema({name: String, type: ['audio', 'lyrics', 'both'], author: Number, lyrics: String, audiofile: String});
-
-    let expl1 = {name: 'Amongus Song', type: 'lyrics', author: 'placeholder', lyrics: 'Hallo Welt'}
-    let expl2 = {name: 'Amongus Song2',type: 'audio', author: 'placeholder', audio: 'BaseEncoded64'}
-    let expl3 = {type: 'audioadd', audio: 'BaseEncoded64', author: 'placeholder', trackid: 'placeholder'}
-    let expl4 = {type: 'lyricsadd', lyrics: 'Hallo Welt', author: 'placeholder', trackid: 'placeholder'}
-    
-    const newSong1 = new Song(expl1);
-    const newSong2 = new Song(expl2);
-    const newSong3 = new Song(expl3);
-    const newSong4 = new Song(expl4);
-
-
-}
 
 function getAudioDir(){
     
