caaf6a9cd5e266cf17353659a4d1505c546b50a3
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-06-19 14:21:36 +0200

    BACKEND: working post api :)

--- express_backend/api_post_new/main.js
+++ express_backend/api_post_new/main.js
@@ -22,13 +22,14 @@ function testCases(){
 }
 
 function getAudioDir(){
-
+    
     const __filename = fileURLToPath(import.meta.url)
-    const __dirname = dirname(__filename)
+    const __dirname = path.dirname(__filename)
 
-    // its best if this solution gets replaced with a less hacky one
-    const __rootDir = path.basename(path.dirname(__dirname))
-    const __audioDir = __rootDir + '/api_audio/audio_files';
+    const __audioDir = path.join(__dirname, '..', 'api_audio', 'audio_files');
+    console.log('Directory to save songs:'+__audioDir)
+
+    //  /usr/src/app/api_audio/audio_files
 
     return __audioDir;
 }
@@ -37,69 +38,85 @@ async function saveAudioFile(audioFileData, name){
     
     // buffer base64 received from request
     const fileContents = Buffer.from(audioFileData, 'base64');
-    
+    console.log(fileContents)
     // find out corresponding filename for data to be written
     const audioFileName = `${getAudioDir()}/${name}.mp3`;
-    
-    writeFile(audioFileName, fileContents, function(err) {
+    console.log('audiofilename: '+audioFileName)
 
-      if(err)console.log(err);
+    let fileNameToBeReferenced;
+    
+    fs.writeFile(audioFileName, fileContents, function(err) {
+
+      if(err){console.log(err);}
+      else{
       console.log(`file saved to ${audioFileName}`)
 
-      let fileNameToBeReferenced = name+'.mp3'
+      fileNameToBeReferenced = name+'.mp3'
       console.log(`the file will be noted in the db as the following name: ${fileNameToBeReferenced}`)
-      return fileNameToBeReferenced
-    
+        }
     });
+
+    return fileNameToBeReferenced
+
 }
 
 export async function requestPost(req, res){
 
-    let song, audiofilename, resMessage='';
+    console.log(req.body.name)
+
+    let song = {}, audiofilename = '', resMessage='';
 
     switch(req.body.typeOfPost){
         // add new Song in DB with only lyrics
-        case lyrics:
+        case 'lyrics':
             song = new Song({name: req.body.name, type: req.body.type, author: req.body.author, lyrics: req.body.lyrics});
             break;
         
         // add new Song in DB with only audio
-        case audio:
-            audiofilename = await saveAudioFile(req.body.audiodata, req.body.name);
-            song = new Song({name: req.body.name, type: req.body.type, author: req.body.author, audiofile: audiofilename});
+        case 'audio':
+            saveAudioFile(req.body.audiodata, req.body.name)
+            .then(song = new Song({name: req.body.name, type: req.body.type, author: req.body.author, audiofile: req.body.name+'.mp3'}))
+            .then(console.log(song))
             break;
         
         // add new Song in DB with both
-        case both:
-            audiofilename = await saveAudioFile(req.body.audiodata, req.body.name);
-            song = new Song({name: req.body.name, type: req.body.type, author: req.body.author, audiofile: audiofilename, lyrics: req.body.lyrics});
+        case 'both':
+            saveAudioFile(req.body.audiodata, req.body.name)
+            .then(song = new Song({name: req.body.name, type: req.body.type, author: req.body.author, audiofile: req.body.name+'.mp3', lyrics: req.body.lyrics})) 
             break;
 
         // add either lyrics or audio to a already existing song
-        case add:
+        case 'add':
             
             // find corresponding song
-            let song = await Song.findOne({_id: req.body.id}, (err)=>{
+            Song.findOne({_id: req.body.id}, (err,song)=>{
+
                 if(err){
                     console.log('document not found');
                     resMessage='object to be updated not found,'
                 }
+                else{
+                    console.log('document found');
+                    switch(req.body.addType){
+
+                        case 'addLyrics':
+                            song.lyrics = req.body.lyrics;
+                            break;
+        
+                        case 'addAudio':
+                            saveAudioFile(req.body.audiodata, req.body.name)
+                            .then(song.audiofile = req.body.name+'.mp3')
+                            break;
+                        
+                    }
+                    
+                    song.type = 'both';
+
+
+                }
             })
 
-            switch(req.body.addType){
 
-                case 'addLyrics':
-                    song.lyrics = req.body.lyrics;
-                    break;
-
-                case 'addAudio':
-                    audiofilename = await saveAudioFile(req.body.audiodata, req.body.name);
-                    song.audiofile = audiofilename;
-                    break;
-                
-            }
-            
-            song.type = 'both';
             
             break;
         }
@@ -107,9 +124,11 @@ export async function requestPost(req, res){
         song.save((err, doc)=>{
             
             if(err){
-                res.send(400).send({message: resMessage+'saving song unsucessful'});
+                res.status(400).send({message: resMessage+'saving song unsucessful'});
             }
             else{
+                console.log('correctly saved document:')
+                console.log(doc)
                 res.status(200).send({message: resMessage+'saving song sucessful'});
             }
         })
