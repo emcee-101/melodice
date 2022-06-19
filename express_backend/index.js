297cf608cd069089fad178756750d09e72b5a30d
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-06-19 21:50:49 +0200

    Revert "Merge branch 'niklas_work' into 'release'"
    This reverts merge request !12

--- express_backend/index.js
+++ express_backend/index.js
@@ -1,7 +1,6 @@
 import {init as startupLyrics} from './api_lyrics/startup.js'
 import {init as startupMatcher} from './api_matcher/startup.js'
 import {init as startupAudio} from './api_audio/startup.js'
-import {init as startupPost} from './api_post_new/startup.js'
 import {connectMongoose, clearDatabase} from './common_base/common.js'
 import {createNewTestSong} from './common_base/test.js'
 
@@ -12,4 +11,3 @@ createNewTestSong()
 startupLyrics()
 startupMatcher()
 startupAudio()
-startupPost()
