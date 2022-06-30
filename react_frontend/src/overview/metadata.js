1c3e56a5f528b4993023f74e810421581ab8e56f
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-06-30 16:45:51 +0200

    initial metadata and player components

--- react_frontend/src/overview/metadata.js
+++ react_frontend/src/overview/metadata.js
@@ -4,4 +4,16 @@ import React from "react";
 
 
 
-export default function metadata(props) {}
+export default function metadata(props) {
+
+    const [metadata, setMetadata] = useState({});
+    async function getMetadata(){
+
+        // fetch
+
+    }
+
+    return(<></>)
+
+
+}
--- react_frontend/src/overview/player.js
+++ react_frontend/src/overview/player.js
@@ -1,6 +1,28 @@
 import React from "react";
+import ReactAudioPlayer from 'react-audio-player';
 
 
 
 
-export default function player() {}
+
+export default function player(props) {
+
+    const [url, setURL] = useState();
+
+    async function getAudioURL(id){
+
+        fetch('194.94.204.27:10091/audio/'+id)
+        .then(response => response.json())
+        .then(data => {console.log(data); 
+                    setURL(data.audiourl)});
+
+    }
+
+    getAudioURL(props.id)
+
+    return (<ReactAudioPlayer
+                controls
+                src={url} 
+            />
+    )
+}
