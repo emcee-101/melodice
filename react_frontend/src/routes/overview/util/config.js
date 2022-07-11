87d3f2e1f6b3f3676596964c35f875ffcabe8ed0
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-07-11 16:51:54 +0200

    revert ports

--- react_frontend/src/routes/overview/util/config.js
+++ react_frontend/src/routes/overview/util/config.js
@@ -1,4 +1,4 @@
 export var IP = "http://194.94.204.27";
 export var audio_service = ":10091/audio/";
-export var getfulldata_service = ":10096/getfulldata/";
+export var getfulldata_service = ":10098/getfulldata/";
 export var matcher_service = ":10092/matcher/";
