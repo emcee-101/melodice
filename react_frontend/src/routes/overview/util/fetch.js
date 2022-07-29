a2bdcd52aa4e02d244dc1b68bbd6825122e2ccae
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-07-29 15:37:15 +0200

    prepare fetch function for usage with POST

--- react_frontend/src/routes/overview/util/fetch.js
+++ react_frontend/src/routes/overview/util/fetch.js
@@ -3,19 +3,21 @@
 // url  example: "http://localhost:10092/matcher/mongoose"
 // method example: "POST"
 
-export async function standardFetch(url, method='GET'){
+export async function standardFetch(url, method='GET', data = {}){
+
+    let config = {
+        mode: 'cors',
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }
+
+    if((method=='POST'||method=='PUT') && (data))
+        config.body = JSON.stringify(data);
     
-    return fetch(url,
-                        {
-                            mode: 'cors',
-                            method: method,
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'Accept': 'application/json'
-                            }
-                    
-                        
-                        })
+    return fetch(url, config)
                         .then( (response) => { return response.json(); })
                         .then( (jsonObj) => {return jsonObj})
                         .catch((e)=>{(console.log(e)); return {message: "there has been an error"}})
