6efb28d3c19f1a44d0f351ea6a3ee3299b301e9a
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-07-30 14:55:13 +0200

    first effort to make form usable to fetch

--- react_frontend/src/components/Save/save.js
+++ react_frontend/src/components/Save/save.js
@@ -5,32 +5,51 @@ import React, {
 import Button from "react-bootstrap/Button";
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import standardFetch from '../../util/fetch'
+import { IP, post_new_service, audio_service } from '../../util/config'
 
 
   // gets passed the audio data (preferably in mp3)
 export default function Save({parentData}){
 
     let style = {padding: "20px"}
+    let formData = {author: null, name: null, origauthor: null, origtitle: null}
 
     const [isCover, setAsCover] = useState(false);
 
-    function submit(){ /* use ParentData and formInput and Call fetch */}
+    function submit(){ /* use ParentData and formInput and Call fetch */
+
+        let data = {name: formData.name, author: formData.author};
+
+        if (isCover) {
+            data.DBID = dbID
+        
+
+        if(data && data.author && data.name){
+            standardFetch(IP+post_new_service, 'POST', data)
+        }   
+        
+
+
+    }
 
     console.log(isCover + "RENDER TRIGGERED")
 
     return <>
-            <form onSubmit={submit()} id="saveForm" style={style}>
+            <Form onSubmit={submit()} id="saveForm" style={style}>
                 
-                <InputGroup className="mb-3" id="author" >
+                <InputGroup className="mb-3" controlId="author" >
                         <InputGroup.Text id="inputGroup-sizing-default">Your Name:
                             </InputGroup.Text>
                         <Form.Control
+                            type="text" placeholder="Name" onChange={(event) => formData.author=event.target.value}
                             aria-label="Your Name"
                         /> </InputGroup>
-                <InputGroup className="mb-3" id="name" >                            
+                <InputGroup className="mb-3" controlId="name" >                            
                         <InputGroup.Text id="inputGroup-sizing-default">Name of the Song:
                             </InputGroup.Text>
                         <Form.Control
+                            type="text" placeholder="Title" onChange={(event) => formData.name=event.target.value}
                             aria-label="Name Of The Song"
                         />  <br />
                 </InputGroup>
@@ -44,23 +63,25 @@ export default function Save({parentData}){
                     { isCover.toString() }
                 </Button>{' '}<br />
 
-                <InputGroup className="mb-3" id="origauthor" style={{ visibility: isCover ? "visible" : "hidden" }}>
+                <InputGroup className="mb-3" controlId="origauthor" style={{ visibility: isCover ? "visible" : "hidden" }}>
                         <InputGroup.Text id="inputGroup-sizing-default">Original Author:
                             </InputGroup.Text>
                         <Form.Control
+                            type="text"    placeholder="Original Author" onChange={(event) => formData.origauthor=event.target.value}
                             aria-label="Original Author Name"
                         />  <br />
                 </InputGroup>
-                <InputGroup className="mb-3" id="origname" style={{ visibility: isCover ? "visible" : "hidden" }}>
+                <InputGroup className="mb-3" controlId="origname" style={{ visibility: isCover ? "visible" : "hidden" }}>
                     <InputGroup.Text id="inputGroup-sizing-default">Original Title:
                                 </InputGroup.Text>
                             <Form.Control
+                                type="text" placeholder="Song Name"  onChange={(event) => formData.origtitle=event.target.value}
                                 aria-label="Original Song Name"
                             />  <br />
                 </InputGroup>                            <br />
 
                 <Button variant="primary" type="submit">Submit</Button>{' '}
-            </form>
+            </Form>
         </>
 
 
--- react_frontend/src/util/config.js
+++ react_frontend/src/util/config.js
@@ -2,3 +2,4 @@ export var IP = "http://localhost";
 export var audio_service = ":10091/audio/";
 export var getfulldata_service = ":10098/getfulldata/";
 export var matcher_service = ":10092/matcher/";
+export var post_new_service = ":10097/api_post_new/";
