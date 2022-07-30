d5c34cc3dd2cd1865536989b03c19ccbc4776d57
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-07-30 12:58:17 +0200

    basically working forms

--- react_frontend/src/components/Save/save.js
+++ react_frontend/src/components/Save/save.js
@@ -1,10 +1,8 @@
 import React, {
-    useCallback,
     useEffect,
-    useRef,
     useState,
   } from "react";
-import { Button } from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
@@ -12,48 +10,54 @@ import InputGroup from 'react-bootstrap/InputGroup';
   // gets passed the audio data (preferably in mp3)
 export default function Save({parentData}){
 
-    const [isCover, setAsCover] = useState(false);
+    let style = {padding: "20px"}
 
-    useEffect(()=>{/*  code to make components visible that make input of original data possible */},[isCover]);
+    const [isCover, setAsCover] = useState(false);
 
     function submit(){ /* use ParentData and formInput and Call fetch */}
 
+    console.log(isCover + "RENDER TRIGGERED")
+
     return <>
-            <form onsubmit={submit()}>
-                <Button variant="outline-primary" type="input" onClick={()=>{isCover ? setAsCover(false) : setAsCover(true)}}>
-                    { isCover.toString() }
-                </Button>{' '}
-                <InputGroup className="mb-3" id="generalData">
-                        <InputGroup.Text id="inputGroup-sizing-default">
+            <form onSubmit={submit()} id="saveForm" style={style}>
+                
+                <InputGroup className="mb-3" id="author" >
+                        <InputGroup.Text id="inputGroup-sizing-default">Your Name:
                             </InputGroup.Text>
                         <Form.Control
                             aria-label="Your Name"
-                            aria-describedby="inputGroup-sizing-default"
-                        />
-                
-                        <InputGroup.Text id="inputGroup-sizing-default">
+                        /> </InputGroup>
+                <InputGroup className="mb-3" id="name" >                            
+                        <InputGroup.Text id="inputGroup-sizing-default">Name of the Song:
                             </InputGroup.Text>
                         <Form.Control
                             aria-label="Name Of The Song"
-                            aria-describedby="inputGroup-sizing-default"
-                        />
+                        />  <br />
                 </InputGroup>
+                <br />
+                <br />
 
-                <InputGroup className="mb-3" id="coverData" style={{ visibility: isCover ? "visible" : "hidden" }}>
-                        <InputGroup.Text id="inputGroup-sizing-default">
+                Is this Song a cover of a already existing Song?   
+                <br />
+         
+                <Button variant="outline-primary" onClick={()=>{isCover ? setAsCover(false) : setAsCover(true)}}>
+                    { isCover.toString() }
+                </Button>{' '}<br />
+
+                <InputGroup className="mb-3" id="origauthor" style={{ visibility: isCover ? "visible" : "hidden" }}>
+                        <InputGroup.Text id="inputGroup-sizing-default">Original Author:
                             </InputGroup.Text>
                         <Form.Control
-                            aria-label="Original Song Name"
-                            aria-describedby="inputGroup-sizing-default"
-                        />
-                
-                        <InputGroup.Text id="inputGroup-sizing-default">
-                            </InputGroup.Text>
-                        <Form.Control
-                            aria-label="Original Song Author"
-                            aria-describedby="inputGroup-sizing-default"
-                        />
+                            aria-label="Original Author Name"
+                        />  <br />
                 </InputGroup>
+                <InputGroup className="mb-3" id="origname" style={{ visibility: isCover ? "visible" : "hidden" }}>
+                    <InputGroup.Text id="inputGroup-sizing-default">Original Title:
+                                </InputGroup.Text>
+                            <Form.Control
+                                aria-label="Original Song Name"
+                            />  <br />
+                </InputGroup>                            <br />
 
                 <Button variant="primary" type="submit">Submit</Button>{' '}
             </form>
